d8e2d1a2c7c2c8bb3833c3863e3165e2a0b0a484
anju16031998 <anju16031998@gmail.com>
2025-11-10 21:46:32 -0500

    Commit to check pipeline

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud():
-    return 'Hello Cloud!'
+    return 'Hello Cloud From Anju!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
